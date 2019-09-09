65a18fe05e41ebab3c88128f25f29f2baf597024
Norihiro Hayashida <nrhr.616@gmail.com>
2019-09-09 20:01:06 +0900

    textカラムにはデフォルト値の設定ができないので修正

--- database/migrations/2019_09_05_230059_create_posts_table.php
+++ database/migrations/2019_09_05_230059_create_posts_table.php
@@ -34,7 +34,7 @@ class CreatePostsTable extends Migration
             $table->string('title')->nullable(false);
 
             // 本文
-            $table->longText('content')->default('');
+            $table->longText('content');
 
             // 投稿状態 nullを許可しない
             // publish: 公開済み

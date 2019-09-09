33e3ec52b66c8ddaae570ad0603dd2dea45ffe68
Norihiro Hayashida <nrhr.616@gmail.com>
2019-09-09 15:38:42 +0900

    postsテーブルスキーマのアップロードメディアについてのコメント追記

--- database/migrations/2019_09_05_230059_create_posts_table.php
+++ database/migrations/2019_09_05_230059_create_posts_table.php
@@ -30,25 +30,31 @@ class CreatePostsTable extends Migration
             $table->foreign('user_id')->references('id')->on('users');
 
             // タイトル デフォルトの255文字まで nullを許可しない
+            // アップロードメディアの場合ファイル名を保存
             $table->string('title')->nullable(false);
 
             // 本文
             $table->longText('content')->default('');
 
             // 投稿状態 nullを許可しない
-            // 'publish': 公開済み
-            // 'draft': 下書き
+            // publish: 公開済み
+            // draft: 下書き
+            // inherit: 画像などのアップロードメディア
             // 2つのどちらかのみを登録する
             $table->string('status', 10)->nullable(false);
 
             // スラッグ(URLの一部になる文字列) nullを許可しない
             // 記事ごとのURLを決定する
             // 例) http://example.com/article/slug
+            // アップロードメディアの場合には保存したファイルのパスを保存
+            // 例) ドキュメントルートが/var/www/laravel/publicで、/var/www/laravel/public/upload配下にファイルを保存する場合
+            // /upload/2019/08/attachment.jpegなどとなる
             $table->string('slug', 100)->nullable(false)->index();
 
             // 投稿種別 nullを許可しない デフォルト'article'
             // article: 記事
             // page: 固定ページ(ブログ紹介ページや自己紹介ページ等の記事以外)
+            // attachment: 画像などのアップロードメディア
             $table->string('type', 15)->default('article')->nullable(false);
             // slugカラムとtypeカラムで複合一意キーを設定
             // これで同一URLの記事を作成しないように制限する
@@ -56,6 +62,7 @@ class CreatePostsTable extends Migration
 
             // MIME_TYPEの設定
             // 通常の記事の場合は'text/html'
+            // 画像などのアップロードメディアの場合は'image/png'や'image/jpeg'など
             $table->string('mime_type', 50)->nullable(false);
 
             // created_at・updated_at

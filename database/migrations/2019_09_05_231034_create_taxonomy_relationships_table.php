6178bfea89eaca9a5df6f1469ee39599f026ecbf
Norihiro Hayashida <nrhr.616@gmail.com>
2019-09-09 20:01:34 +0900

    中間テーブルスキーマ作成

--- database/migrations/2019_09_05_231034_create_taxonomy_relationships_table.php
+++ database/migrations/2019_09_05_231034_create_taxonomy_relationships_table.php
@@ -22,7 +22,22 @@ class CreateTaxonomyRelationshipsTable extends Migration
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_bin';
 
-            $table->bigIncrements('id');
+            // タクソノミーid (外部キー)
+            $table->unsignedBigInteger('taxonomy_id')->nullable(false);
+            // タクソノミーidの外部キー指定
+            // これでtaxonomy_idとtaxonomyテーブルのidを紐付けて外部キーとして設定する
+            $table->foreign('taxonomy_id')->references('id')->on('taxonomy');
+
+            // 記事id (外部キー)
+            $table->unsignedBigInteger('post_id')->nullable(false);
+            // 記事idの外部キー指定
+            // これでpost_idとtaxonomyテーブルのidを紐付けて外部キーとして設定する
+            $table->foreign('post_id')->references('id')->on('posts');
+
+            // タクソノミーページ表示時の表示順番号
+            // 対象の記事が追加されていくごとに番号を増やして登録する
+            $table->unsignedBigInteger('taxonomy_order')->nullable(false)->default(1);
+
             $table->timestamps();
         });
     }
